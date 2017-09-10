06baea25b485a3e3c7c3512b644fb31138d7260a
tingshui <798309758@qq.com>
2017-09-10 14:29:43 -0500

    first commit

---
+++ HW5/routes/videos.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var router = express.Router();
+
+var monk = require('monk');
+var db = monk('localhost:27017/vidzy');
+
+/* function(req,res) is a call back function, it is the actual implementation of router.
+db.get-- database, conncet to database
+{} --- get all
+*/
+router.get('/', function(req, res) {
+    var collection = db.get('videos');
+    collection.find({}, function(err, videos){
+        if (err) throw err;
+      	res.json(videos);
+    });
+});
+/* we need to use post to insert, save*/
+router.post('/', function(req, res){
+    var collection = db.get('videos');
+    collection.insert({
+        title: req.body.title,
+        description: req.body.description
+    }, function(err, video){
+        if (err) throw err;
+
+        res.json(video);
+    });
+});
+
+router.get('/:id', function(req, res) {
+    var collection = db.get('videos');
+    collection.findOne({ _id: req.params.id }, function(err, video){
+        if (err) throw err;
+
+      	res.json(video);
+    });
+});
+
+router.put('/:id', function(req, res){
+    var collection = db.get('videos');
+    collection.update({
+        _id: req.params.id
+    },
+    {
+        title: req.body.title,
+        description: req.body.description
+    }, function(err, video){
+        if (err) throw err;
+
+        res.json(video);
+    });
+});
+
+router.delete('/:id', function(req, res){
+    var collection = db.get('videos');
+    collection.remove({ _id: req.params.id }, function(err, video){
+        if (err) throw err;
+
+        res.json(video);
+    });
+});
+module.exports = router;
